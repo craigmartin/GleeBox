7de89a0932a191c26e1048e994f482c85656c032
craigmartin <craig@thegleemachine.com>
2012-02-22 14:53:29 -0800

    add_rails_app

---
+++ gleebox/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Gleebox::Application.config.secret_token = '040792189d131637c5ab6c1d53ba870fcce27813e145438c465caf75671ed0cd4c60471e332502c905f1dc5951be256db36f90449afb1316bcaceb21b471a268'
